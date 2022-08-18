242f5029f97bf378ea3c5a793d3e5b43bef787b5
amarioguy <arminders208@outlook.com>
2022-08-17 22:41:45 -0400

    AIC - add AIC driver INF/C file
    AIC driver is now officially being worked on
    
    Signed-off-by: amarioguy <arminders208@outlook.com>

---
+++ Platform/M1Pkg/Drivers/AppleAicDxe/AppleAicDxe.c
@@ -0,0 +1,17 @@
+/**
+ * @file AppleAicDxe.c
+ * @author amarioguy (arminders208@outlook.com)
+ * 
+ * This file implements the selector logic for the AIC DXE Driver.
+ * 
+ * @version 1.0
+ * @date 2022-08-16
+ * 
+ * @copyright Copyright (c) amarioguy (Arminder Singh) 2022.
+ * 
+ * Credits to the Asahi Linux contributors for the initial AIC implementations in m1n1 and linux.
+ * 
+ * SPDX-License-Identifier: BSD-2-Clause-Patent
+ * 
+ */
+
