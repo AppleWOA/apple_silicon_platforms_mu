68774e89faf9613a5c260acfacfa7d3332a13f19
amarioguy <arminders208@outlook.com>
2022-08-17 22:44:37 -0400

    PSCI - refactor Apple HVC PSCI calling code
    was using an outdated base, now it's based off the current version of the SMC PSCI reset system library and the compiler won't complain :)
    
    Signed-off-by: amarioguy <arminders208@outlook.com>

--- Platform/M1Pkg/Library/AppleArmPsciHvcResetLib/AppleArmPsciHvcResetSystemLib.c
+++ Platform/M1Pkg/Library/AppleArmPsciHvcResetLib/AppleArmPsciHvcResetSystemLib.c
@@ -2,13 +2,14 @@
  * @file AppleArmPsciHvcResetSystemLib.c
    Support PSCI reset system functionality on Apple platforms
 
-   Note: based on ArmPkg/Library/ArmPsciResetSystemLib
+   Note: based on ArmPkg/Library/ArmSmcPsciResetSystemLib
 
    The primary difference in this PSCI library is that on Apple platforms,
-   EL3 is unimplemented, so we need to use m1n1/the hypervisor layer
-   to implement PSCI methods like system reset
+   EL3 is unimplemented, so if possible it's best to stick to HVC calls to handle PSCI
 
-   If we are running in EL2, we will need to instead issue a "genter" instruction to enter GL2 (guarded EL2, Apple's idea of secure world)
+   NOTE: if EL2 can handle SMC exceptions, then this entire library will no longer be necessary and in that case this implementation will use the standard ArmSmcPsciResetSystemLib
+
+   If we are running in EL2, we will instead issue a "genter" instruction to enter GL2 (guarded EL2, Apple's idea of secure world)
    and implement PSCI methods there.
 
    Note that this will assume that SPRR/GXF were set up before entry into UEFI and behavior of genter without this set up is undefined.
@@ -30,35 +31,130 @@
 
 #include <IndustryStandard/ArmStdSmc.h>
 
-EFI_STATUS EFIAPI LibResetSystem(IN EFI_RESET_TYPE SysResetType, OUT EFI_STATUS ResetStatus, IN UINTN DataSize, IN VOID *ResetData OPTIONAL)
+//TODO: implement GXF support
+
+/**
+  This function causes a system-wide reset (cold reset), in which
+  all circuitry within the system returns to its initial state. This type of reset
+  is asynchronous to system operation and operates without regard to
+  cycle boundaries.
+
+  If this function returns, it means that the system does not support cold reset.
+**/
+VOID
+EFIAPI
+ResetCold (
+  VOID
+  )
 {
+  ARM_HVC_ARGS ColdResetArgs;
+  ColdResetArgs.Arg0 = ARM_SMC_ID_PSCI_SYSTEM_RESET;
+  ColdResetArgs.Arg1 = 0;
+  ColdResetArgs.Arg2 = 0;
+  ColdResetArgs.Arg3 = 0;
+  // Send a PSCI 0.2 SYSTEM_RESET command
+  ArmCallHvc(&ColdResetArgs);
+}
 
-  //TODO: implement GXF support
-  ARM_HVC_ARGS PsciHvcArgs;
+/**
+  This function causes a system-wide initialization (warm reset), in which all processors
+  are set to their initial state. Pending cycles are not corrupted.
 
-  switch (SysResetType)
-  {
-    
-    // fall through to cold reboot for warm and platform specific reboots (warm can eventually be implemented)
+  If this function returns, it means that the system does not support warm reset.
+**/
+VOID
+EFIAPI
+ResetWarm (
+  VOID
+  )
+{
+  // Map a warm reset into a cold reset
+  ResetCold ();
+}
+
+/**
+  This function causes the system to enter a power state equivalent
+  to the ACPI G2/S5 or G3 states.
+
+  If this function returns, it means that the system does not support shutdown reset.
+**/
+VOID
+EFIAPI
+ResetShutdown (
+  VOID
+  )
+{
+  ARM_HVC_ARGS ShutdownArgs;
+  ShutdownArgs.Arg0 = ARM_SMC_ID_PSCI_SYSTEM_RESET;
+  ShutdownArgs.Arg1 = 0;
+  ShutdownArgs.Arg2 = 0;
+  ShutdownArgs.Arg3 = 0;
+  // Send a PSCI 0.2 SYSTEM_OFF command
+  ArmCallHvc(&ShutdownArgs);
+}
+
+/**
+  This function causes a systemwide reset. The exact type of the reset is
+  defined by the EFI_GUID that follows the Null-terminated Unicode string passed
+  into ResetData. If the platform does not recognize the EFI_GUID in ResetData
+  the platform must pick a supported reset type to perform.The platform may
+  optionally log the parameters from any non-normal reset that occurs.
+
+  @param[in]  DataSize   The size, in bytes, of ResetData.
+  @param[in]  ResetData  The data buffer starts with a Null-terminated string,
+                         followed by the EFI_GUID.
+**/
+VOID
+EFIAPI
+ResetPlatformSpecific (
+  IN UINTN  DataSize,
+  IN VOID   *ResetData
+  )
+{
+  // Map the platform specific reset as reboot
+  ResetCold ();
+}
+
+
+/**
+  The ResetSystem function resets the entire platform.
+
+  @param[in] ResetType      The type of reset to perform.
+  @param[in] ResetStatus    The status code for the reset.
+  @param[in] DataSize       The size, in bytes, of ResetData.
+  @param[in] ResetData      For a ResetType of EfiResetCold, EfiResetWarm, or EfiResetShutdown
+                            the data buffer starts with a Null-terminated string, optionally
+                            followed by additional binary data. The string is a description
+                            that the caller may use to further indicate the reason for the
+                            system reset.
+**/
+VOID
+EFIAPI
+ResetSystem (
+  IN EFI_RESET_TYPE  ResetType,
+  IN EFI_STATUS      ResetStatus,
+  IN UINTN           DataSize,
+  IN VOID            *ResetData OPTIONAL
+  )
+{
+  switch (ResetType) {
     case EfiResetWarm:
-    case EfiResetPlatformSpecific:
+      ResetWarm ();
+      break;
+
     case EfiResetCold:
-      PsciHvcArgs.Arg0 = ARM_SMC_ID_PSCI_SYSTEM_RESET;
+      ResetCold ();
       break;
+
     case EfiResetShutdown:
-      PsciHvcArgs.Arg0 = ARM_SMC_ID_PSCI_SYSTEM_OFF;
-      break;
+      ResetShutdown ();
+      return;
+
+    case EfiResetPlatformSpecific:
+      ResetPlatformSpecific (DataSize, ResetData);
+      return;
+
     default:
-      // the requested PSCI system reset functionality is not implemented
-      DEBUG ((DEBUG_ERROR, "AppleArmPsciResetSystemLib: PSCI Method not implemented\n"));
-    
-    //Make the hypercall to perform the PSCI action requested in arg0
-    //TODO: implement a selector mechanism based on current EL, just use standard Hvc for now
-
-    ArmCallHvc(&PsciHvcArgs);
-
-    //if we come here, something has gone *very* wrong
-    DEBUG ((DEBUG_ERROR, "AppleArmPsciResetSystemLib: PSCI method failed, looping in place...\n"));
-    CpuDeadLoop();
+      return;
   }
 }
