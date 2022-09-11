40b25955b2f76ecb9cd7e9f1f3033b7d03e8052b
amarioguy (Arminder Singh) <arminders208@outlook.com>
2022-09-11 17:01:04 -0400

    add required libraries to M1v2 platform package
    Signed-off-by: amarioguy (Arminder Singh) <arminders208@outlook.com>

---
+++ Platform/M1v2Pkg/Library/MsPlatformDevicesLib/MsPlatformDevicesLib.c
@@ -0,0 +1,90 @@
+/** @file
+ *MsPlatformDevicesLib  - Device specific library.
+
+Copyright (C) Microsoft Corporation. All rights reserved.
+SPDX-License-Identifier: BSD-2-Clause-Patent
+
+**/
+
+#include <Uefi.h>
+#include <Library/DeviceBootManagerLib.h>
+#include <Library/MsPlatformDevicesLib.h>
+
+/**
+Library function used to provide the platform SD Card device path
+**/
+EFI_DEVICE_PATH_PROTOCOL *
+EFIAPI
+GetSdCardDevicePath (
+  VOID
+  )
+{
+  return NULL;
+}
+
+/**
+  Library function used to determine if the DevicePath is a valid bootable 'USB' device.
+  USB here indicates the port connection type not the device protocol.
+  With TBT or USB4 support PCIe storage devices are valid 'USB' boot options.
+**/
+BOOLEAN
+EFIAPI
+PlatformIsDevicePathUsb (
+  IN EFI_DEVICE_PATH_PROTOCOL  *DevicePath
+  )
+{
+  return FALSE;
+}
+
+/**
+Library function used to provide the list of platform devices that MUST be
+connected at the beginning of BDS
+**/
+EFI_DEVICE_PATH_PROTOCOL **
+EFIAPI
+GetPlatformConnectList (
+  VOID
+  )
+{
+  return NULL;
+}
+
+/**
+ * Library function used to provide the list of platform console devices.
+ */
+BDS_CONSOLE_CONNECT_ENTRY *
+EFIAPI
+GetPlatformConsoleList (
+  VOID
+  )
+{
+  return NULL;
+}
+
+/**
+Library function used to provide the list of platform devices that MUST be connected
+to support ConsoleIn activity.  This call occurs on the ConIn connect event, and
+allows platforms to do enable specific devices ConsoleIn support.
+**/
+EFI_DEVICE_PATH_PROTOCOL **
+EFIAPI
+GetPlatformConnectOnConInList (
+  VOID
+  )
+{
+  return NULL;
+}
+
+/**
+Library function used to provide the console type.  For ConType == DisplayPath,
+device path is filled in to the exact controller to use.  For other ConTypes, DisplayPath
+must NULL. The device path must NOT be freed.
+**/
+EFI_HANDLE
+EFIAPI
+GetPlatformPreferredConsole (
+  OUT EFI_DEVICE_PATH_PROTOCOL  **DevicePath
+  )
+{
+  return NULL;
+}
