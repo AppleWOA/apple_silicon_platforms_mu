4fffa933f9c97b5d53f8211ad45bcc45a30f5447
amarioguy (Arminder Singh) <arminders208@outlook.com>
2022-09-25 12:43:31 -0400

    simplify MIDR check
    Signed-off-by: amarioguy (Arminder Singh) <arminders208@outlook.com>

--- Platform/AppleSiliconPkg/Library/ArmGicArchLib/ArmGicArchLib.c
+++ Platform/AppleSiliconPkg/Library/ArmGicArchLib/ArmGicArchLib.c
@@ -32,7 +32,13 @@ ArmGicArchLibInitialize (
   // feature is implemented on the CPU. This is also convenient as our GICv3
   // driver requires SRE. If only Memory mapped access is available we try to
   // drive the GIC as a v2.
-  if (ArmHasGicSystemRegisters ()) {
+  
+  // Apple silicon platform change:
+  // Apple chips will not report GIC support in ID_AA64PFR0_EL1 despite implementing
+  // the GIC CPU interface and MSRs in hardware. As a workaround for this, check if the
+  // vendor ID in MIDR_EL1 (bits 31-24) is 0x61 (implementer code for Apple), and
+  // verify using the ICC_SRE method just in case. (unknown if chips prior to the M1 implement any parts of GIC at all)
+  if (ArmHasGicSystemRegisters () || (Midr & 0x61000000) ) {
     // Make sure System Register access is enabled (SRE). This depends on the
     // higher privilege level giving us permission, otherwise we will either
     // cause an exception here, or the write doesn't stick in which case we need
@@ -51,25 +57,6 @@ ArmGicArchLibInitialize (
       goto Done;
     }
   }
-  // Apple silicon platform change:
-  // Apple chips will not report GIC support in ID_AA64PFR0_EL1 despite implementing
-  // the GIC CPU interface and MSRs in hardware. As a workaround for this, check if the
-  // vendor ID in MIDR_EL1 (bits 31-24) is 0x61 (implementer code for Apple), and
-  // verify using the ICC_SRE method just in case. (unknown if chips prior to the M1 implement any parts of GIC at all)
-  else if (Midr & 0x61000000)
-  {
-    IccSre = ArmGicV3GetControlSystemRegisterEnable ();
-    if (!(IccSre & ICC_SRE_EL2_SRE)) {
-      ArmGicV3SetControlSystemRegisterEnable (IccSre | ICC_SRE_EL2_SRE);
-      IccSre = ArmGicV3GetControlSystemRegisterEnable ();
-    }
-
-    if (IccSre & ICC_SRE_EL2_SRE) {
-      mGicArchRevision = ARM_GIC_ARCH_REVISION_3;
-      goto Done;
-    }
-  }
-
   mGicArchRevision = ARM_GIC_ARCH_REVISION_2;
 
 Done:
