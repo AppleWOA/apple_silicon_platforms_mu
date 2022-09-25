0d4356d20a51125d5188f4e2eb27630886bbd739
amarioguy (Arminder Singh) <arminders208@outlook.com>
2022-09-25 12:43:53 -0400

    begin filling out AicDxe header file
    Signed-off-by: amarioguy (Arminder Singh) <arminders208@outlook.com>

---
+++ Platform/AppleSiliconPkg/Drivers/AppleAicDxe/AppleAicDxe.h
@@ -0,0 +1,20 @@
+
+/**
+ * @file AppleAicDxe.h
+ * @author amarioguy (Arminder Singh)
+ * @brief AIC DXE Driver Header File.
+ * 
+ * @version 1.0
+ * @date 2022-09-24
+ * 
+ * @copyright Copyright (c) amarioguy (Arminder Singh) 2022.
+ * 
+ */
+
+#include <Library/DebugLib.h>
+#include <Library/IoLib.h>
+#include <Library/MemoryAllocationLib.h>
+#include <Library/UefiBootServicesTableLib.h>
+#include <Library/UefiLib.h>
+#include <Library/ArmLib.h>
+
