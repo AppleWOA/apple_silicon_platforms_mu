456a95ff81c9da3c9585344dcc6e79010c68595a
amarioguy (Arminder Singh) <arminders208@outlook.com>
2022-10-06 21:25:56 -0400

    add common DXE driver code file for AIC DXE driver
    Signed-off-by: amarioguy (Arminder Singh) <arminders208@outlook.com>

---
+++ Platform/AppleSiliconPkg/Drivers/AppleAicDxe/AppleAicCommonDxe.c
@@ -0,0 +1,15 @@
+/**
+ * @file AppleAicCommonDxe.c
+ * @author amarioguy (Arminder Singh)
+ * 
+ * AIC DXE Driver code common to both AICv1 and AICv2
+ * @version 1.0
+ * @date 2022-10-06
+ * 
+ * @copyright Copyright (c) amarioguy (Arminder Singh), 2022.
+ * 
+ * SPDX-License-Identifier: BSD-2-Clause-Patent
+ * 
+ */
+
+#include "AppleAicDxe.h"
