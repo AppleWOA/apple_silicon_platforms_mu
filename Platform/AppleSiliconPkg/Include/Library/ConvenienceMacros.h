6bf04700f1b514ba29d3dd7429485955edc18c0a
amarioguy (Arminder Singh) <arminders208@outlook.com>
2022-10-10 08:05:34 -0400

    add ConvenienceMacros header file
    macros used for convenience/readability reasons will be added here
    
    Signed-off-by: amarioguy (Arminder Singh) <arminders208@outlook.com>

---
+++ Platform/AppleSiliconPkg/Include/Library/ConvenienceMacros.h
@@ -0,0 +1,30 @@
+/**
+ * @file ConvenienceMacros.h
+ * @author amarioguy (Arminder Singh)
+ * 
+ * Macros used for convenience/readability reasons in the code.
+ * Some are borrowed from Linux or m1n1 (for example to help with bitmasks)
+ * @version 1.0
+ * @date 2022-10-09
+ * 
+ * @copyright Copyright (c) amarioguy (Arminder Singh), 2022.
+ * 
+ * SPDX-License-Identifier: BSD-2-Clause-Patent
+ * 
+ */
+
+#ifndef CONVENIENCE_MACROS_H
+#define CONVENIENCE_MACROS_H
+
+//borrowed from m1n1
+#define BIT(x) (1UL << (x))
+#define GENMASK(msb, lsb) ((BIT((msb + 1) - (lsb)) - 1) << (lsb))
+
+//While EDK2 does have code that reads bitfields from values, the m1n1 bitfield macros
+//seem easier to deal with, so put these in the code anyways
+#define _FIELD_LSB(field)      ((field) & ~(field - 1))
+
+#define FIELD_PREP(field, val) ((val) * (_FIELD_LSB(field)))
+#define FIELD_GET(field, val)  (((val) & (field)) / _FIELD_LSB(field))
+
+#endif //CONVENIENCE_MACROS_H
