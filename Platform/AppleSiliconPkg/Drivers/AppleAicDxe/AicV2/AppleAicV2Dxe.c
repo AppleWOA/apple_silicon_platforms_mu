9e3a6da3fecdcb62482a01f9b694c4cf1108a68e
amarioguy (Arminder Singh) <arminders208@outlook.com>
2022-10-11 22:44:24 -0400

    add FDT library, move register offset calculations to a separate function
    Signed-off-by: amarioguy (Arminder Singh) <arminders208@outlook.com>

--- Platform/AppleSiliconPkg/Drivers/AppleAicDxe/AicV2/AppleAicV2Dxe.c
+++ Platform/AppleSiliconPkg/Drivers/AppleAicDxe/AicV2/AppleAicV2Dxe.c
@@ -14,6 +14,7 @@
  */
 
 #include <Library/AppleAicLib.h>
+#include <Include/libfdt.h>
 #include "AppleAicDxe.h"
 
 STATIC UINT64 AicV2Base;
@@ -41,6 +42,29 @@ STATIC EFI_STATUS EFIAPI AppleAicV2MaskInterrupt(
 }
 
 /**
+ * Calculate the AIC register offsets on the platform
+ * 
+ * @return EFI_SUCCESS unconditionally.
+ */
+EFI_STATUS AppleAicV2CalculateRegisterOffsets(IN VOID)
+{
+    //needed for devicetree calculations
+    UINT32 SocNode;
+    UINT32 InterruptControllerNode;
+
+    //Start with simple things
+    AicV2Base = PcdGet64(PcdAicInterruptControllerBase);
+    AicInfoStruct->NumIrqs = AppleAicGetNumInterrupts(AicV2Base);
+    AicInfoStruct->MaxIrqs = AppleAicGetMaxInterrupts(AicV2Base);
+    AicInfoStruct->MaxCpuDies = FIELD_GET(AIC_V2_INFO_REG3_MAX_DIE_COUNT_BITFIELD, MmioRead32(AicV2Base + AIC_V2_INFO_REG3));
+    AicInfoStruct->NumCpuDies = FIELD_GET(AIC_V2_INFO_REG1_LAST_CPU_DIE_BITFIELD, MmioRead32(AicV2Base + AIC_V2_INFO_REG1));
+    
+    //Now for the more complicated bits...
+    //Start by getting the event register offset from the devicetree
+    //TODO: event reg, die stride, set/clear regs
+}
+
+/**
  * Prepares the AIC protocol for use by the DXE environment.
  * 
  * @param ImageHandle
@@ -58,10 +82,15 @@ EFI_STATUS AppleAicV2DxeInit(IN EFI_HANDLE ImageHandle, IN EFI_SYSTEM_TABLE *Sys
     //Assert that the Hardware Interrupt protocol is not installed already.
     ASSERT_PROTOCOL_ALREADY_INSTALLED (NULL, &gHardwareInterruptProtocolGuid);
 
-    AicV2Base = PcdGet64(PcdAicInterruptControllerBase);
-    AicInfoStruct->NumIrqs = AicV2NumInterrupts = AppleAicGetNumInterrupts(AicV2Base);
-    AicInfoStruct->MaxIrqs = AicV2MaxInterrupts = AppleAicGetMaxInterrupts(AicV2Base);
-    DEBUG((DEBUG_INFO, "AICv2 with %d/%d configured IRQs, at 0x%llx\n", AicV2NumInterrupts, AicV2MaxInterrupts, AicV2Base));
+    //set up and collect variables in a global struct that holds AIC register offsets and useful values.
+    //this is the approach m1n1 and Linux use
+    Status = AppleAicV2CalculateRegisterOffsets();
+    AicV2NumInterrupts = AicInfoStruct->NumIrqs;
+    AicV2MaxInterrupts = AicInfoStruct->MaxIrqs;
+    
+    DEBUG((DEBUG_INFO, "AICv2 with %u/%u configured IRQs, at 0x%llx\n", AicV2NumInterrupts, AicV2MaxInterrupts, AicV2Base));
+    DEBUG((DEBUG_INFO, "%u/%u CPU dies, Die Stride: 0x%lx", AicInfoStruct->NumCpuDies, AicInfoStruct->MaxCpuDies, AicInfoStruct->DieStride));
+    DEBUG((DEBUG_VERBOSE, "AIC register addresses: \n"));
 
     //start from a clean state by disabling all interrupts
     for(InterruptIndex = 0; InterruptIndex < AicV2NumInterrupts; InterruptIndex++)
