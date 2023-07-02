90d097a8755e31c02d4d076b05da71b7fcf22f70
Arminder Singh (amarioguy) <arminders208@outlook.com>
2023-07-01 22:19:18 -0400

    update MsPlatformDevicesLib for T600x platforms
    Signed-off-by: Arminder Singh (amarioguy) <arminders208@outlook.com>

--- Silicon/Apple/T600XFamilyPkg/Library/MsPlatformDevicesLib/MsPlatformDevicesLib.c
+++ Silicon/Apple/T600XFamilyPkg/Library/MsPlatformDevicesLib/MsPlatformDevicesLib.c
@@ -7,8 +7,62 @@ SPDX-License-Identifier: BSD-2-Clause-Patent
 **/
 
 #include <Uefi.h>
+
+#include <Protocol/DevicePath.h>
+
+#include <Library/BaseMemoryLib.h>
+#include <Library/DebugLib.h>
 #include <Library/DeviceBootManagerLib.h>
+#include <Library/DevicePathLib.h>
+#include <Library/IoLib.h>
 #include <Library/MsPlatformDevicesLib.h>
+#include <Library/PcdLib.h>
+#include <Library/UefiBootServicesTableLib.h>
+#include <Library/UefiLib.h>
+
+typedef struct {
+  VENDOR_DEVICE_PATH       DisplayDevicePath;
+  EFI_DEVICE_PATH_PROTOCOL EndDevicePath;
+} EFI_DISPLAY_DEVICE_PATH;
+
+EFI_DISPLAY_DEVICE_PATH DisplayDevicePath =
+{
+  {
+    {
+      HARDWARE_DEVICE_PATH,
+      HW_VENDOR_DP,
+      {
+        (UINT8)(sizeof(VENDOR_DEVICE_PATH)),
+        (UINT8)((sizeof(VENDOR_DEVICE_PATH)) >> 8)
+      }
+    },
+    EFI_GRAPHICS_OUTPUT_PROTOCOL_GUID
+  },
+  {
+    END_DEVICE_PATH_TYPE,
+    END_ENTIRE_DEVICE_PATH_SUBTYPE,
+    {
+      (UINT8)(END_DEVICE_PATH_LENGTH),
+      (UINT8)((END_DEVICE_PATH_LENGTH) >> 8)
+    }
+  }
+};
+//
+// Predefined platform default console device path
+//
+BDS_CONSOLE_CONNECT_ENTRY gPlatformConsoles[] =
+{
+  {
+    (EFI_DEVICE_PATH_PROTOCOL *)&DisplayDevicePath,
+    CONSOLE_OUT | STD_ERROR
+  },
+  {
+    NULL,
+    0
+  }
+};
+
+EFI_DEVICE_PATH_PROTOCOL *gPlatformConInDeviceList[] = {NULL};
 
 /**
 Library function used to provide the platform SD Card device path
@@ -58,7 +112,7 @@ GetPlatformConsoleList (
   VOID
   )
 {
-  return NULL;
+  return (BDS_CONSOLE_CONNECT_ENTRY *)&gPlatformConsoles;
 }
 
 /**
@@ -86,5 +140,37 @@ GetPlatformPreferredConsole (
   OUT EFI_DEVICE_PATH_PROTOCOL  **DevicePath
   )
 {
-  return NULL;
+  EFI_STATUS                Status;
+  EFI_HANDLE                Handle = NULL;
+  EFI_DEVICE_PATH_PROTOCOL *TempDevicePath;
+
+  TempDevicePath = (EFI_DEVICE_PATH_PROTOCOL *)&DisplayDevicePath;
+
+  Status = gBS->LocateDevicePath(
+      &gEfiGraphicsOutputProtocolGuid, &TempDevicePath, &Handle);
+  if (!EFI_ERROR(Status) && IsDevicePathEnd(TempDevicePath)) {
+  }
+  else {
+    DEBUG(
+        (DEBUG_ERROR,
+         "%a - Unable to locate platform preferred console. Code=%r\n",
+         __FUNCTION__, Status));
+    Status = EFI_DEVICE_ERROR;
+  }
+
+  if (Handle != NULL) {
+    //
+    // Connect the GOP driver
+    //
+    gBS->ConnectController(Handle, NULL, NULL, TRUE);
+
+    //
+    // Get the GOP device path
+    // NOTE: We may get a device path that contains Controller node in it.
+    //
+    TempDevicePath = EfiBootManagerGetGopDevicePath(Handle);
+    *DevicePath    = TempDevicePath;
+  }
+
+  return Handle;
 }
