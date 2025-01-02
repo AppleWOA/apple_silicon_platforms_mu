144bb5ed4a1ac705b6127b97eac8e8126f0b39a1
Arminder Singh (amarioguy) <arminders208@outlook.com>
2025-01-02 18:55:15 -0500

    fixup ACPI tables
    Signed-off-by: Arminder Singh (amarioguy) <arminders208@outlook.com>

--- Silicon/Apple/AppleSiliconPkg/Drivers/AcpiPlatformDxe/AcpiPlatform.c
+++ Silicon/Apple/AppleSiliconPkg/Drivers/AcpiPlatformDxe/AcpiPlatform.c
@@ -223,6 +223,103 @@ LocateFvInstanceWithSocTables (
 }
 
 /**
+  Locate the first instance of a protocol.  If the protocol requested is an
+  FV protocol, then it will return the first FV that contains the SoC-specific ACPI table
+  storage file.
+
+  @param  Instance      Return pointer to the first instance of the protocol
+
+  @return EFI_SUCCESS           The function completed successfully.
+  @return EFI_NOT_FOUND         The protocol could not be located.
+  @return EFI_OUT_OF_RESOURCES  There are not enough resources to find the protocol.
+
+**/
+EFI_STATUS
+LocateFvInstanceWithGenericTables (
+  OUT EFI_FIRMWARE_VOLUME2_PROTOCOL  **GenericInstance
+  )
+{
+  EFI_STATUS                     Status;
+  EFI_HANDLE                     *HandleBuffer;
+  UINTN                          NumberOfHandles;
+  EFI_FV_FILETYPE                FileType;
+  UINT32                         FvStatus;
+  EFI_FV_FILE_ATTRIBUTES         Attributes;
+  UINTN                          Size;
+  UINTN                          Index;
+  EFI_FIRMWARE_VOLUME2_PROTOCOL  *FvInstance;
+
+  FvStatus = 0;
+
+  //
+  // Locate protocol.
+  //
+  Status = gBS->LocateHandleBuffer (
+                  ByProtocol,
+                  &gEfiFirmwareVolume2ProtocolGuid,
+                  NULL,
+                  &NumberOfHandles,
+                  &HandleBuffer
+                  );
+  if (EFI_ERROR (Status)) {
+    //
+    // Defined errors at this time are not found and out of resources.
+    //
+    return Status;
+  }
+
+  //
+  // Looking for FV with device-specific ACPI table storage file
+  //
+
+  for (Index = 0; Index < NumberOfHandles; Index++) {
+    //
+    // Get the protocol on this handle
+    // This should not fail because of LocateHandleBuffer
+    //
+    Status = gBS->HandleProtocol (
+                    HandleBuffer[Index],
+                    &gEfiFirmwareVolume2ProtocolGuid,
+                    (VOID **)&FvInstance
+                    );
+    ASSERT_EFI_ERROR (Status);
+
+    //
+    // See if it has the ACPI storage file
+    //
+    Status = FvInstance->ReadFile (
+                           FvInstance,
+                           (EFI_GUID *)PcdGetPtr (PcdGenericAcpiTableStorageFile),
+                           NULL,
+                           &Size,
+                           &FileType,
+                           &Attributes,
+                           &FvStatus
+                           );
+
+    //
+    // If we found it, then we are done
+    //
+    if (Status == EFI_SUCCESS) {
+      *GenericInstance = FvInstance;
+      break;
+    }
+  }
+
+  //
+  // Our exit status is determined by the success of the previous operations
+  // If the protocol was found, Instance already points to it.
+  //
+
+  //
+  // Free any allocated buffers
+  //
+  gBS->FreePool (HandleBuffer);
+
+  return Status;
+}
+
+/**
   This function calculates and updates an UINT8 checksum.
 
   @param  Buffer          Pointer to buffer to checksum
@@ -367,10 +464,76 @@ AcpiPlatformEntryPoint (
   //
   // Locate the firmware volume protocol
   //
-  DEBUG((DEBUG_ERROR, "%a: Locating SoC specific ACPI tables\n", __FUNCTION__));
-  Status = LocateFvInstanceWithSocTables (&FwVol2);
+  // DEBUG((DEBUG_ERROR, "%a: Locating SoC specific ACPI tables\n", __FUNCTION__));
+  // Status = LocateFvInstanceWithSocTables (&FwVol2);
+  // if (EFI_ERROR (Status)) {
+  //   DEBUG((DEBUG_ERROR, "%a: Failed to locate SoC specific tables, status %r (error code %llx)\n", __FUNCTION__, Status, Status));
+  //   ASSERT(FALSE);
+  //   return EFI_ABORTED;
+  // }
+
+  // //
+  // // Read tables from the storage file.
+  // //
+  // while (Status == EFI_SUCCESS) {
+  //   Status = FwVol2->ReadSection (
+  //                     FwVol2,
+  //                     (EFI_GUID *)PcdGetPtr (PcdSocAcpiTableStorageFile),
+  //                     EFI_SECTION_RAW,
+  //                     Instance,
+  //                     (VOID **)&CurrentTable,
+  //                     &Size,
+  //                     &FvStatus
+  //                     );
+  //   if (!EFI_ERROR (Status)) {
+  //     //
+  //     // Add the table
+  //     //
+  //     TableHandle = 0;
+
+  //     TableSize = ((EFI_ACPI_DESCRIPTION_HEADER *)CurrentTable)->Length;
+  //     ASSERT (Size >= TableSize);
+
+  //     //
+  //     // Checksum ACPI table
+  //     //
+  //     AcpiPlatformChecksum ((UINT8 *)CurrentTable, TableSize);
+
+  //     //
+  //     // Install ACPI table
+  //     //
+  //     Status = AcpiTable->InstallAcpiTable (
+  //                           AcpiTable,
+  //                           CurrentTable,
+  //                           TableSize,
+  //                           &TableHandle
+  //                           );
+
+  //     //
+  //     // Free memory allocated by ReadSection
+  //     //
+  //     gBS->FreePool (CurrentTable);
+
+  //     if (EFI_ERROR (Status)) {
+  //       return EFI_ABORTED;
+  //     }
+
+  //     //
+  //     // Increment the instance
+  //     //
+  //     Instance++;
+  //     CurrentTable = NULL;
+  //   }
+  // }
+
+
+  //
+  // Locate the firmware volume protocol
+  //
+  DEBUG((DEBUG_ERROR, "%a: Locating generic ACPI tables\n", __FUNCTION__));
+  Status = LocateFvInstanceWithGenericTables (&FwVol2);
   if (EFI_ERROR (Status)) {
-    DEBUG((DEBUG_ERROR, "%a: Failed to locate SoC specific tables, status %r (error code %llx)\n", __FUNCTION__, Status, Status));
+    DEBUG((DEBUG_ERROR, "%a: Failed to locate generic tables, status %r (error code %llx)\n", __FUNCTION__, Status, Status));
     ASSERT(FALSE);
     return EFI_ABORTED;
   }
@@ -381,7 +544,7 @@ AcpiPlatformEntryPoint (
   while (Status == EFI_SUCCESS) {
     Status = FwVol2->ReadSection (
                       FwVol2,
-                      (EFI_GUID *)PcdGetPtr (PcdSocAcpiTableStorageFile),
+                      (EFI_GUID *)PcdGetPtr (PcdGenericAcpiTableStorageFile),
                       EFI_SECTION_RAW,
                       Instance,
                       (VOID **)&CurrentTable,
@@ -428,6 +591,7 @@ AcpiPlatformEntryPoint (
       CurrentTable = NULL;
     }
   }
+
 
   //
   // The driver does not require to be kept loaded.
